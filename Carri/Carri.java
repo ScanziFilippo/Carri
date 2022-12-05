ab5017f5e8e832c1c78a5e16d1a44db1699ea59f
ScanziFilippo <101709401+ScanziFilippo@users.noreply.github.com>
2022-12-05 12:32:06 +0100

    0.1
    Versione 0.1

---
+++ Carri/Carri.java
@@ -0,0 +1,15 @@
+public class Carri
+{
+    private int x;
+    public Carri()
+    {
+        // inizializza le variabili d'istanza
+        x = 0;
+    }
+    
+    public int sampleMethod(int y)
+    {
+        // metti qui il tuo codice
+        return x + y;
+    }
+}
